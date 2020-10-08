5b6a5be9048b813332e6ba831a2520cd9d24c7ee
repl.it user <replituser@example.com>
2020-10-08 06:05:18 +0000

    Updates

---
+++ precache-manifest.ab0fdcf78dc2244dc2db5afdfdae1459.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44ac9bc8b212130c7f67e4386d15969",
+    "url": "/ZTM-React/index.html"
+  },
+  {
+    "revision": "dc079e918f7177d28bac",
+    "url": "/ZTM-React/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "995f201df11116ccbc5c",
+    "url": "/ZTM-React/static/css/main.bc014031.chunk.css"
+  },
+  {
+    "revision": "dc079e918f7177d28bac",
+    "url": "/ZTM-React/static/js/2.5d46d9da.chunk.js"
+  },
+  {
+    "revision": "995f201df11116ccbc5c",
+    "url": "/ZTM-React/static/js/main.a183086a.chunk.js"
+  },
+  {
+    "revision": "9da3c386fbff593640d3",
+    "url": "/ZTM-React/static/js/runtime-main.4f7f6c93.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ZTM-React/static/media/SEGA.12a3db8b.woff"
+  }
+]);
